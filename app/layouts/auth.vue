f8a1792c84704dd54514bbafeeba83a8bbabe02c
Muhammed <baslm8496@gmail.com>
2025-10-23 13:27:22 +0300

    Refactor auth layout and create default layout
    - Simplify auth layout to white background without default content
    - Create new default layout with original slate background and shared content

--- app/layouts/auth.vue
+++ app/layouts/auth.vue
@@ -1,6 +1,5 @@
 <template>
-    <div class="bg-slate-200 h-screen">
-        <p>Some default layout content shared across all pages</p>
+    <div class="bg-white h-screen">
         <slot />
     </div>
 </template>
