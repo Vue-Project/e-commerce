3c373d40282c3ba0254e88298885671af0f188be
Muhammed <baslm8496@gmail.com>
2025-11-11 14:30:06 +0200

    Add admin pages and navigation icons

---
+++ app/pages/admin/categories.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="bg-slate-200 h-screen">
+        <h1>categories</h1>
+    </div>
+</template>
+
+<script setup>
+definePageMeta({
+    layout: "admin",
+});
+</script>
+
+<style lang="scss" scoped></style>
