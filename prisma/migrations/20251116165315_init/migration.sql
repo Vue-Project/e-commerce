c581e921ab2f9cf1684a94e16bd6be036ba15a5f
Muhammed <baslm8496@gmail.com>
2025-11-16 19:20:29 +0200

    Add Category model to Prisma schema

--- prisma/migrations/20251116165315_init/migration.sql
+++ prisma/migrations/20251116165315_init/migration.sql
@@ -10,5 +10,16 @@ CREATE TABLE "User" (
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
