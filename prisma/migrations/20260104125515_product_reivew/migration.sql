a8ab68eb948011a68bd46ec2f2b1f1053c38be4f
Muhammed <baslm8496@gmail.com>
2026-01-07 16:19:26 +0200

    Add ProductReview model and user role field to schema

---
+++ prisma/migrations/20260104125515_product_reivew/migration.sql
@@ -0,0 +1,21 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "role" TEXT;
+
+-- CreateTable
+CREATE TABLE "ProductReview" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "starNumber" INTEGER NOT NULL,
+    "productId" INTEGER NOT NULL,
+    "comment" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ProductReview_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "ProductReview" ADD CONSTRAINT "ProductReview_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProductReview" ADD CONSTRAINT "ProductReview_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
